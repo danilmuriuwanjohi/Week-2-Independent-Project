fecd651cbbc5fb92f91fac37a5a439646ec75e61
danilmuriuwanjohi <danrich50@gmail.com>
2021-06-13 21:43:53 +0300

    added statement

--- independent-project.js
+++ independent-project.js
@@ -3,9 +3,9 @@ var gender = prompt ("enter your gender");
 var CC = prompt ("enter your cc");
 var YY = prompt ("enter yy");
 var MM = prompt ("Enter MM");
-var DD = prompt ("Enter your dd")
+var DD = prompt ("Enter your dd");
 var mod = prompt ("Enter your mod. Please enter in decimal.");
 var age = function () {
     (((CC/4) - 2*CC - 1) + (5* YY/4) + (26* (MM + 1))/10 +DD)*mod;
+    console.log ("The number of the day of the week in which you were born is" + age);
 };
-console.log ("The number of the day of the week in which you were born is" + age);
